681af6175d09b13428f687d80a2087008178f2b7
dimpllepatel <49346188+dimpllepatel@users.noreply.github.com>
2020-07-31 16:20:28 -0500

    Update and rename adminusers.sql to users.sql

--- adminusers.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM studcertdb.adminusers;
-
-INSERT INTO studcertdb.adminusers (firstname, lastname, username, password)
-Values ('Bill', 'Nye' , 'Bill2' ,'admin1');
---
+++ users.sql
@@ -0,0 +1,4 @@
+SELECT * FROM studcertdb.users;
+
+INSERT INTO studcertdb.users (ID, username, password, active)
+Values (1, 'john' , '123456' , 1);
